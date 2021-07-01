a0e2a552e2cd534dae185484538f9b0a353f6156
MateusSantosF <mateusferreira.dev@gmail.com>
2021-07-01 12:29:14 -0300

    Add ConnectionMVC

--- src/BeutifulSalon/dao/ConnectionMVC.java
+++ src/BeutifulSalon/dao/ConnectionMVC.java
@@ -32,23 +32,23 @@ public class ConnectionMVC {
              Connection conn = null;
           
             try {
-                //NAO APAGAR PELO AMOR DE DEUS!
-//            //recupera path de onde o .jar foi executado para indicar caminho do banco
-//            String path = ConnectionMVC.class.getProtectionDomain().getCodeSource().getLocation().getPath();
-//            String decodedPath = URLDecoder.decode(path, "UTF-8");
-//            String caminhoBanco = decodedPath.replace("BeutifulSalon.jar", "BancoDeDados/beutifulsalondb.db");
-//     
-//            conn = DriverManager.getConnection("jdbc:sqlite:"+caminhoBanco); 
+//                //NAO APAGAR PELO AMOR DE DEUS!
+////            //recupera path de onde o .jar foi executado para indicar caminho do banco
+            String path = ConnectionMVC.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+            String decodedPath = URLDecoder.decode(path, "UTF-8");
+           String caminhoBanco = decodedPath.replace("BeutifulSalon.jar", "BancoDeDados/beutifulsalondb.db");
+////     
+            conn = DriverManager.getConnection("jdbc:sqlite:"+caminhoBanco); 
               
                
                 //UTILIZAR ENQUANTO NO NETBEANS
-                conn = DriverManager.getConnection("jdbc:sqlite:BancoDeDados/beutifulsalondb.db");
+                //conn = DriverManager.getConnection("jdbc:sqlite:BancoDeDados/beutifulsalondb.db");
                
             } catch (SQLException e) {
                 JOptionPane.showMessageDialog(null, "erro: " + e);
                 
-            //} catch (UnsupportedEncodingException ex) {
-            //Logger.getLogger(ConnectionMVC.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (UnsupportedEncodingException ex) {
+            Logger.getLogger(ConnectionMVC.class.getName()).log(Level.SEVERE, null, ex);
         }
       
         return conn;
